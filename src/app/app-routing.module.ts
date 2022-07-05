2aa604eb206579ac0c0c9d67f60fc28b4e21c643
Alberto Tontoni <tontonialberto@outlook.it>
2022-07-05 12:54:24 +0200

    Configure resolver to fetch data for the recipe edit component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
   { path: 'recipes', component: RecipeListComponent, children: [
     { path: '', component: ErrorPageComponent, data: { message: 'Please select a recipe!' }},
     { path: ':id', component: RecipeDetailComponent, resolve: { recipe: RecipeResolver } },
-    { path: ':id/edit', component: RecipeEditComponent }
+    { path: ':id/edit', component: RecipeEditComponent, resolve: { recipe: RecipeResolver } }
   ] },
   { path: '**', component: ErrorPageComponent, data: { message: 'Oops.. this page does not exist!' } }
 ];
--- src/app/recipe-list/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe-list/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Data } from '@angular/router';
+import { Recipe } from 'src/app/_models/recipe.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeEditComponent implements OnInit {
 
-  constructor() { }
+  recipe: Recipe = null;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.data.subscribe(
+      (data: Data) => {
+        this.recipe = data.recipe;
+        console.log(this.recipe)
+      }
+    );
   }
 
 }
