80cb815e57843359b68fdcbcbb20cf10124fcd43
tontonialberto <tontonialberto@outlook.it>
2022-07-11 02:37:30 +0200

    Add forgotten unsubscribe

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Recipe } from '../_models/recipe.model';
 import { RecipeService } from '../_services/recipe.service';
 
@@ -7,18 +8,24 @@ import { RecipeService } from '../_services/recipe.service';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
 
   recipes: Recipe[] = [];
 
-  constructor(private recipeService: RecipeService) { }
+  private subRecipesChanged: Subscription;
 
+  constructor(private recipeService: RecipeService) { }
+  
   ngOnInit(): void {
     this.recipes = this.recipeService.getAll();
-    this.recipeService.recipesChanged.subscribe(
+    this.subRecipesChanged = this.recipeService.recipesChanged.subscribe(
       (recipes: Recipe[]) => {
         this.recipes = recipes;
       }
     )
+  }
+
+  ngOnDestroy(): void {
+    this.subRecipesChanged.unsubscribe();
   }
 }
