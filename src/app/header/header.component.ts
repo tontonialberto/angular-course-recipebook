a482ece32fb90e6c2d5cfe765fd16d94a12ae1cf
tontonialberto <tontonialberto@outlook.it>
2022-07-15 12:38:31 +0200

    Update nav links according to login status

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { User } from '../_models/user.model';
+import { AuthService } from '../_services/auth.service';
 import { RecipeService } from '../_services/recipe.service';
 
 @Component({
@@ -12,7 +14,13 @@ export class HeaderComponent implements OnInit {
 
   openDropdown: boolean = false;
 
-  constructor(private recipeService: RecipeService) { }
+  isLoggedIn: boolean = false;
+
+  constructor(private recipeService: RecipeService, private authService: AuthService) {
+    this.authService.user.subscribe((user: User) => {
+      this.isLoggedIn = (user !== null && user.token !== null);
+    });
+  }
 
   ngOnInit(): void {
   }
